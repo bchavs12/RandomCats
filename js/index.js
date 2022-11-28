5ea095d34c16def452e2a296b759c17f12482312
bchavs12 <chavesbruno699@gmail.com>
2022-11-27 21:43:56 -0300

    first commit

--- js/index.js
+++ js/index.js
@@ -1,22 +1,30 @@
 // Aqui realizamos a consulta da promisse, esperando sua resposta assíncrona
-fetch('https://randomuser.me/api/')
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        //manipulamos a resposta
-        console.log(data)
-    });
+fetch('https://api.thecatapi.com/v1/images/search?category_ids=4&limit=1')
+  .then((response) => {
+    return response.json()
+  })
+  .then((cats) => {
+    renderizar(cats)
+  })
 
-function renderizarDadosUsuario(dados) {
-    /* -------------------------------- Tarefa 1 -------------------------------- */
-    // Aqui devem desenvolver uma função que seja exibida na tela:
-    // a foto, o nome completo do usuário e o e-mail.
-    // Isto deve ser baseado nas informações que obtemos da API e inseridas no HTML.
+let img = document.createElement('img')
+
+function renderizar(dados) {
+  let container = document.getElementById('container')
+  let divImg = document.createElement('div')
+  divImg.classList.add('divImg')
+  img.src = dados[0].url
+  divImg.appendChild(img)
+  container.appendChild(divImg)
 }
 
-
-/* --------------------------- Tarefa 2 (extra) --------------------------- */
-// Aqui você pode ir para o ponto extra de usar o botão que está comentado no HTML.
-// Você pode descomentar o código no index.html e usar esse botão para executar uma nova solicitação API, sem recarregar a página.
-// Cabe aos desenvolvedores decidirem qual bloco de código deve ser contido dentro de uma função para que ele possa ser executado toda vez que um clique de botão for realizado.
+function atualizar() {
+  fetch('https://api.thecatapi.com/v1/images/search?category_ids=4&limit=1')
+    .then((response) => {
+      return response.json()
+    })
+    .then((cats) => {
+      console.log(cats)
+      img.src = cats[0].url
+    })
+}
